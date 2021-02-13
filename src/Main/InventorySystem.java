557246aa2f05578c1dde104ab9aa516594db63e6
Chance Crawford <chancecmontano@gmail.com>
2021-02-13 15:56:59 -0600

    Main part/prod tables populate with inventory data now

--- src/Main/InventorySystem.java
+++ src/Main/InventorySystem.java
@@ -15,10 +15,20 @@ public class InventorySystem extends Application {
     }
 
     @Override
-    public void start(Stage primaryStage) throws Exception{
+    public void start(Stage primaryStage) throws Exception {
+        createInventory(inventory);
+
         Parent root = FXMLLoader.load(getClass().getResource("/ViewControllers/MainForm.fxml"));
         Scene scene = new Scene(root);
         primaryStage.setScene(scene);
         primaryStage.show();
+    }
+
+    void createInventory(Inventory inventory) {
+        Inventory.addPart(new InHouse(01, "IH Part 1", 3.50, 5, 1, 10, 55));
+        Inventory.addPart(new Outsourced(03, "OS Part 1", 4.50, 3, 1, 20, "Streisshand"));
+
+        Inventory.addProduct(new Product(15, "Prod 01", 5.25, 6, 1, 8));
+        Inventory.addProduct(new Product(18, "Prod 02", 2.25, 9, 1, 12));
     }
 }
--- src/ViewControllers/MainFormController.java
+++ src/ViewControllers/MainFormController.java
@@ -1,4 +1,80 @@
 package ViewControllers;
 
+import Classes.Inventory;
+import Classes.Part;
+import Classes.Product;
+
+import javafx.fxml.FXML;
+import javafx.scene.Parent;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.stage.Stage;
+
 public class MainFormController {
+    Stage stage;
+    Parent scene;
+
+    // initializing FXML elements for:
+    // Parts table
+    @FXML
+    private TextField MainPartSearch;
+    @FXML
+    private TableView<Part> MainPartTable;
+    @FXML
+    private TableColumn<Part, Integer> partIdColumn;
+    @FXML
+    private TableColumn<Part, String> partNameColumn;
+    @FXML
+    private TableColumn<Part, Integer> partInventoryColumn;
+    @FXML
+    private TableColumn<Part, Double> partPriceCostColumn;
+    @FXML
+    private Button MainPartAdd;
+    @FXML
+    private Button MainPartModify;
+    @FXML
+    private Button MainPartDelete;
+    // Products table
+    @FXML
+    private TextField MainProductSearch;
+    @FXML
+    private TableView<Product> MainProductTable;
+    @FXML
+    private TableColumn<Product, Integer> productIdColumn;
+    @FXML
+    private TableColumn<Product, String> productNameColumn;
+    @FXML
+    private TableColumn<Product, Integer> productInventoryColumn;
+    @FXML
+    private TableColumn<Product, Double> productPriceCostColumn;
+    @FXML
+    private Button MainProductAdd;
+    @FXML
+    private Button MainProductModify;
+    @FXML
+    private Button MainProductDelete;
+    @FXML
+    private Button MainExitButton;
+
+    @FXML
+    private void initialize() {
+        // populate parts table with data from Inventory System
+        MainPartTable.getItems().addAll(Inventory.getAllParts());
+        partIdColumn.setCellValueFactory(new PropertyValueFactory<>("id"));
+        partNameColumn.setCellValueFactory(new PropertyValueFactory<>("name"));
+        partInventoryColumn.setCellValueFactory(new PropertyValueFactory<>("stock"));
+        partPriceCostColumn.setCellValueFactory(new PropertyValueFactory<>("price"));
+
+        // populate product table with data from Inventory System
+        MainProductTable.getItems().addAll(Inventory.getAllProducts());
+        productIdColumn.setCellValueFactory(new PropertyValueFactory<>("id"));
+        productNameColumn.setCellValueFactory(new PropertyValueFactory<>("name"));
+        productInventoryColumn.setCellValueFactory(new PropertyValueFactory<>("stock"));
+        productPriceCostColumn.setCellValueFactory(new PropertyValueFactory<>("price"));
+    }
+
+
 }
