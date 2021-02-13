e87f9a03501203684baac182fb729ac36fbb66c1
Chance Crawford <chancecmontano@gmail.com>
2021-02-13 09:33:37 -0600

    Removing unneeded files/classes

--- src/Main/InventorySystem.java
+++ src/Main/InventorySystem.java
@@ -5,19 +5,20 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import Classes.*;
 
 public class InventorySystem extends Application {
-
-    @Override
-    public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
-        primaryStage.show();
-    }
-
+    Inventory inventory = new Inventory();
 
     public static void main(String[] args) {
         launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("/ViewControllers/MainForm.fxml"));
+        Scene scene = new Scene(root);
+        primaryStage.setScene(scene);
+        primaryStage.show();
     }
 }
